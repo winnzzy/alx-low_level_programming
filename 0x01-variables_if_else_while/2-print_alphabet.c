1d200e10202dbca583e5039ad0c913a36c2a7d52
winnzzy <nnamuahn@gmail.com>
2022-12-14 14:24:24 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,12 @@
 /**
  * main - prints alphabets in lowercase
  * Return: 0
- */
+ **/
 
 int main(void)
 {
 	char 1_letter;
+	
 	for (1_letter = 97; 1_letter <= 122; 1_letter++)
 	{
 		putchar(1_letter);
